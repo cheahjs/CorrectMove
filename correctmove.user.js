4f550530598834333db367e1f7855e59f53d1c97
Andrew Shand <Andrew@andrews-mbp.home>
2016-05-10 21:51:54 +0100

    Parse dates from agent paragraph if necessary
    Also, handle pagination better

--- correctmove.user.js
+++ correctmove.user.js
@@ -39,6 +39,10 @@ const once = _.once(function($) {
         const link = window.location.origin + jcard.find('.propertyCard-link').attr('href');
         const idContainer = jcard.find('.propertyCard-anchor');
         const id = idContainer.attr('id');
+        
+        // Reset anything we've done to the card
+        jcard.parent().css('opacity', 1);
+        jcard.find('.betterMove').remove();
 
         function addToCard(card, time) {
 
@@ -56,7 +60,6 @@ const once = _.once(function($) {
             }
 
             const element = `<span class="betterMove">${text}, </span>`;
-            card.find('.betterMove').remove();
             card.find('.propertyCard-branchSummary-addedOrReduced').prepend(element);
             
             if (options.hideBeforeDate) {
@@ -74,23 +77,68 @@ const once = _.once(function($) {
 
             $.get(link, function(data, request) {
 
-                $(data).find('#lettingInformation tr').each((index, row) => {
-                    if ($(row).text().indexOf('available') >= 0) {
+                function findDateAsMoment() {
+                    
+                    const html = $(data);
+                    
+                    // First try get from the default place
+                    const potentialDate = html.find('#lettingInformation tr').map((index, row) => {
+                        if ($(row).text().indexOf('available') >= 0) {
 
-                        const dateText = $(row).children()[1].innerText.trim();
+                            const dateText = $(row).children()[1].innerText.trim();
 
-                        let time;
-                        if (dateText.toLowerCase() == 'now') {
-                            time = moment();
+                            let time;
+                            if (dateText.toLowerCase() == 'now') {
+                                time = moment();
+                            }
+                            else {
+                                time = moment(dateText, "DD/MM/YYYY");
+                            }
+
+                            return time;
                         }
-                        else {
-                            time = moment(dateText, "DD/MM/YYYY");
+                    }).filter(el => typeof(el) !== 'undefined').toArray().shift();
+                    
+                    if (moment.isMoment(potentialDate)) {
+                        return potentialDate;
+                    }                  
+                    
+                    const regex = /([0-9][0-9]?(th|rd|st|nd)*) (may|june|july|sep|sept|september|oct|october|nov|november|dec|december|jan|january)/;
+                    const searchText = html.find('.agent-content :not(#lettingInformation).sect').text().toLowerCase()
+                    const textDate = searchText.match(regex).shift();
+                    
+                    if (typeof(textDate) === 'string') {
+                        function isValid(mmnt) {
+                            return !isNaN(mmnt.toDate().getTime());
                         }
-
-                        GM_setValue("homer-" + id, time.unix() * 1000);
-                        addToCard(jcard, time.unix() * 1000);
+                        
+                        let date = moment(textDate.trim());
+                        if (isValid(date)) {
+                            return date;
+                        }   
+                        
+                        date = moment(textDate.trim(), 'Do MMMM');
+                        if (isValid(date)) {
+                            return date;
+                        }
+                        
+                        date = moment(textDate.trim(), 'D MMMM');
+                        if (isValid(date)) {
+                            return date;
+                        }
                     }
-                });
+                    
+                    const availableNow = searchText.indexOf('available now') >= 0;
+                    if (availableNow === true) {
+                        return moment();
+                    }
+                }
+                
+                const time = findDateAsMoment();
+                if (moment.isMoment(time)) {
+                    GM_setValue("homer-" + id, time.unix() * 1000);
+                    addToCard(jcard, time.unix() * 1000);
+                }                
             });
 
         }, (1000 * i) + (2500 * Math.random()));
