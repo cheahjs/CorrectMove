1422b1be37f5f4f63c592743f14c5ac37a5480d6
Andrew Shand <andrewshand94@gmail.com>
2016-07-30 07:02:50 +0100

    Bump version

--- correctmove.user.js
+++ correctmove.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CorrectMove
 // @namespace    http://andrewshand.github.io/
-// @version      0.2.2
+// @version      0.2.3
 // @description  Make it easier to find a property on RightMove
 // @author       andrewshand94
 // @connect      *
